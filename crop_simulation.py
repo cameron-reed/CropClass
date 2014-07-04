ce78f3f4431497448e116d7e4ef02712cfcec2a3
cameron-reed <cameron.reeed@gmail.com>
2014-07-04 10:24:49 +0100

    finished event driven tasks

--- crop_simulation.py
+++ crop_simulation.py
@@ -5,6 +5,9 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 from radio_button_widget_class import * #provides the radio button widget
+from manual_grow_dialog_class import * #provides the manual grow dialog window
+from crop_view_class import * #provides the graphical crop status display
+
 from wheat_class import *
 from potato_class import *
 
@@ -53,6 +56,17 @@ class CropWindow(QMainWindow):
         self.days_line_edit = QLineEdit()
         self.status_line_edit = QLineEdit()
 
+        if crop_type == 1:
+            self.crop_view = WheatView()
+        elif crop_type == 2:
+            self.crop_view = PotatoView()
+
+        #ensure the crop view appears a certain size
+        self.crop_view.setHorizontalScrollBarPolicy(1)
+        self.crop_view.setVerticalScrollBarPolicy(1)
+        self.crop_view.setFixedHeight(182)
+        self.crop_view.setFixedWidth(242)
+
         self.manual_grow_button = QPushButton("Manually Grow")
         self.automatic_grow_button = QPushButton("Automatically Grow")
 
@@ -70,6 +84,7 @@ class CropWindow(QMainWindow):
         self.status_grid.addWidget(self.status_line_edit,2,1)
 
         #add widgets/layouts to the grow layout
+        self.grow_grid.addWidget(self.crop_view,0,0)
         self.grow_grid.addLayout(self.status_grid,0,1)
         self.grow_grid.addWidget(self.manual_grow_button,1,0)
         self.grow_grid.addWidget(self.automatic_grow_button,1,1)
@@ -80,6 +95,7 @@ class CropWindow(QMainWindow):
 
         #connections
         self.automatic_grow_button.clicked.connect(self.automatically_grow_crop)
+        self.manual_grow_button.clicked.connect(self.manually_grow_crop)
         
 
     def instantiate_crop(self):
@@ -99,6 +115,13 @@ class CropWindow(QMainWindow):
             water = random.randint(1,10)
             self.simulated_crop.grow(light,water)
         self.update_crop_view_status()
+
+    def manually_grow_crop(self):
+        manual_values_dialog = ManualGrowDialog()
+        manual_values_dialog.exec_() #run the dialog window
+        light, water = manual_values_dialog.values()
+        self.simulated_crop.grow(light,water)
+        self.update_crop_view_status()
         
     def update_crop_view_status(self):
         crop_status_report = self.simulated_crop.report() #get the crop report
@@ -107,6 +130,17 @@ class CropWindow(QMainWindow):
         self.growth_line_edit.setText(str(crop_status_report["growth"]))
         self.days_line_edit.setText(str(crop_status_report["days growing"]))
         self.status_line_edit.setText(str(crop_status_report["status"]))
+
+        if crop_status_report["status"] == "Seed":
+            self.crop_view.switch_scene(0)
+        elif crop_status_report["status"] == "Seedling":
+            self.crop_view.switch_scene(1)
+        elif crop_status_report["status"] == "Young":
+            self.crop_view.switch_scene(2)
+        elif crop_status_report["status"] == "Mature":
+            self.crop_view.switch_scene(3)
+        elif crop_status_report["status"] == "Old":
+            self.crop_view.switch_scene(4)
 
 if __name__ == "__main__":
     crop_simulation = QApplication(sys.argv) #create new application
--- manual_grow_dialog_class.py
+++ manual_grow_dialog_class.py
@@ -15,3 +15,21 @@ class ManualGrowDialog(QDialog):
 
         self.water_spinbox.setSuffix(" Water")
         self.light_spinbox.setSuffix(" Light")
+
+        self.water_spinbox.setValue(1)
+        self.light_spinbox.setValue(1)
+
+        self.submit_button = QPushButton("Enter Values")
+
+        self.dialog_layout = QVBoxLayout()
+        self.dialog_layout.addWidget(self.light_spinbox)
+        self.dialog_layout.addWidget(self.water_spinbox)
+        self.dialog_layout.addWidget(self.submit_button)
+
+        self.setLayout(self.dialog_layout)
+
+        #connections
+        self.submit_button.clicked.connect(self.close)
+
+    def values(self):
+        return int(self.light_spinbox.value()), int(self.water_spinbox.value())
